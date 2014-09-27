7e630c34a65f65376a9ce9cd25fd9e9987676aaa
jmiller <jmiller@QG245990.qg.com>
2014-09-26 21:11:56 -0500

    Initial commit

---
+++ src/discountstrategy/DiscountStrategy.java
@@ -0,0 +1,12 @@
+package discountstrategy;
+
+/**
+ *
+ * @author jmiller
+ */
+public interface DiscountStrategy {
+
+    
+    public abstract double getDiscount();
+    
+}
---
+++ src/discountstrategy/Startup.java
@@ -0,0 +1,17 @@
+package discountstrategy;
+
+/**
+ *
+ * @author jmiller
+ */
+public class Startup {
+    
+    
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        
+        
+    }
+}
